e98fbb0b9ce88b8c303414d010e89b83a2b6a4e3
leeworkandmisc <90721860+leeworkandmisc@users.noreply.github.com>
2021-09-14 22:21:34 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'IRHC',
   appName: 'Infrared Heating Calculator',
-  webDir: 'https',
+  webDir: 'www',
   bundledWebRuntime: false,
 };
 
